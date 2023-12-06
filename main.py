e2ef8fa5234ed6f2edf235c9d099ebded4a33472
Marco Casciato <cas.marco07@gmail.com>
2023-12-06 10:44:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("500x400")
 
-text_label = tk.Label(root, text="Das ist ein Test!")
+text_label = tk.Label(root, text="Das ist ein Test!", font=("Arial", 16))
 text_label.pack(pady=50)
 text_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
