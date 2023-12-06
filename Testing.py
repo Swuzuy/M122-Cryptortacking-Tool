a597222e835e7fda0d41094da448202231121459
Marco Casciato <cas.marco07@gmail.com>
2023-12-06 10:02:53 +0100

    Test UI
    Testing of UI

--- Testing.py
+++ Testing.py
@@ -1,2 +1,15 @@
-Test
-wirufh
+import tkinter as tk
+
+# Erstelle das Hauptfenster
+root = tk.Tk()
+
+# Funktion zur Anpassung der Größe des Fensters
+def resize_window():
+    root.geometry("400x300")  # Ändere die Größe des Fensters auf 400x300
+
+# Button zur Größenanpassung
+resize_button = tk.Button(root, text="Größe anpassen", command=resize_window)
+resize_button.pack()  # Zeige den Button im Fenster an
+
+# Starte die UI-Schleife
+root.mainloop()
